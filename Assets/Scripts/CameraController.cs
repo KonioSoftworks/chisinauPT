9a204275c8cd0d39c852b2a8e1189eb8c8c92d6c
Maxian Nicu <maxiannicu@gmail.com>
2014-11-06 22:53:53 +0200

    some improvements

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public float carDistance = 12;
+
+	// Update is called once per frame
+	void Update () {
+		var car = GameObject.FindGameObjectWithTag("Player");
+		Vector3 carV = car.transform.position;
+		Vector3 camV = transform.position;
+		transform.position = new Vector3(carV.x - carDistance,camV.y,camV.z);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	public List<WheelCollider> forwardWheels;
+
+	public List<WheelCollider> backWheels;
+
+	public float motorTorque = 10f;
+
+	public float steerAngle = 20f;
+
+	public float brakeTorque = 50f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		setSteerAngle(steerAngle * x);
+		if(y > 0){
+			//setBrake(0);
+			setTorque(motorTorque*y);
+		} else if(y < 0){
+			//setBrake(brakeTorque*y);
+		}
+
+	}
+
+	void setTorque(float value){
+		for(int i=0;i < forwardWheels.Count;i++){
+			forwardWheels[i].motorTorque = motorTorque;
+		}
+	}
+
+	void setSteerAngle(float value){
+		for(int i=0;i < forwardWheels.Count;i++){
+			forwardWheels[i].steerAngle = value;
+		}
+	}
+
+	void setBrake(float value){
+		for(int i=0;i < forwardWheels.Count;i++){
+			forwardWheels[i].brakeTorque = value;
+		}
+		for(int i=0;i < backWheels.Count;i++){
+			backWheels[i].brakeTorque = value;
+		}
+	}	
+}
