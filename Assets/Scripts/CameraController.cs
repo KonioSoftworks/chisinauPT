795fd75f6e0522d4015a8e8a39ecf3e11bd711e3
Maxian Nicu <maxiannicu@gmail.com>
2014-11-08 13:42:45 +0200

    road lamps added

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CameraController : MonoBehaviour {
 
-	public float carDistance = 12;
+	public float carDistance = 12f;
+
+	public GameObject tile;
+
+	public int tileRendered = 10;
+
+	public float distanceBetweenTiles = 10f;
+
+	private List<GameObject> tiles;
+
+	private float distance = 0f;
+
+	void Start () {
+		tiles = new List<GameObject>();
+		GameObject Road = GameObject.FindGameObjectWithTag("Road");
+		for(int i=0;i<tileRendered;i++){
+			Vector3 newPosition = new Vector3(tile.transform.position.x,tile.transform.position.y,
+			                                  distance);
+			distance += distanceBetweenTiles;
+			tiles.Add((GameObject)Instantiate(tile,newPosition,tile.transform.rotation));
+			tiles[i].transform.parent = Road.transform;
+		}
+	}
 
 	// Update is called once per frame
 	void Update () {
+		// move camera
 		var car = GameObject.FindGameObjectWithTag("Player");
 		Vector3 carV = car.transform.position;
 		Vector3 camV = transform.position;
-		transform.position = new Vector3(carV.x - carDistance,camV.y,camV.z);
+		transform.position = new Vector3(camV.x,camV.y,carV.z - carDistance);
+
+		// moving tiles
+		for(int i=0;i < tiles.Count;i++){
+			if(!tiles[i].renderer.isVisible && tiles[i].transform.position.z < carV.z - distanceBetweenTiles){
+				Vector3 newPosition = new Vector3(tiles[i].transform.position.x,tiles[i].transform.position.y,
+				                                  distance);
+				tiles[i].transform.position = newPosition;
+				//Debug.Log (distance);
+				distance += distanceBetweenTiles;
+			}
+		}
 	}
+
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,34 +8,31 @@ public class PlayerController : MonoBehaviour {
 
 	public List<WheelCollider> backWheels;
 
-	public float motorTorque = 10f;
+	public float motorTorque = 30f;
 
-	public float steerAngle = 20f;
+	public float steerAngle = 10f;
 
 	public float brakeTorque = 50f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	public float maxSpeed = 50f;
+
 	// Update is called once per frame
 	void FixedUpdate () {
-		float x = Input.GetAxis("Horizontal");
-		float y = Input.GetAxis("Vertical");
-		setSteerAngle(steerAngle * x);
-		if(y > 0){
-			//setBrake(0);
-			setTorque(motorTorque*y);
-		} else if(y < 0){
-			//setBrake(brakeTorque*y);
-		}
 
+		float steer = Mathf.Clamp(Input.GetAxis("Horizontal"), -1, 1);
+		float motor = Mathf.Clamp(Input.GetAxis("Vertical"), 0, 1);
+		float brake = -1 * Mathf.Clamp(Input.GetAxis("Vertical"), -1, 0);
+
+		setSteerAngle(steer * steerAngle);
+		setTorque(motorTorque * motor);
+		setBrake(brake * brakeTorque);
+
+		audio.pitch = 1 + (forwardWheels[forwardWheels.Count-1].motorTorque) / motorTorque;
 	}
 
 	void setTorque(float value){
 		for(int i=0;i < forwardWheels.Count;i++){
-			forwardWheels[i].motorTorque = motorTorque;
+			forwardWheels[i].motorTorque = value;
 		}
 	}
 
@@ -49,6 +46,7 @@ public class PlayerController : MonoBehaviour {
 		for(int i=0;i < forwardWheels.Count;i++){
 			forwardWheels[i].brakeTorque = value;
 		}
+
 		for(int i=0;i < backWheels.Count;i++){
 			backWheels[i].brakeTorque = value;
 		}
