edcf33f4d6f617ee7a7f466972228c6149a3b393
Maxian Nicu <maxiannicu@gmail.com>
2014-11-11 19:19:26 +0200

    saved

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -8,6 +9,68 @@ public class PlayerController : MonoBehaviour {
 
 	public List<WheelCollider> backWheels;
 
+	//new phisics
+
+	public List<float> GearRatios;
+
+	public float axleRatio;
+
+	public float minRpm;
+
+	public float maxRpm;
+
+	public float wheelRadius;
+
+	public float hp;
+
+	private float rpm;
+
+	private int gear;
+
+	private float converterRatio = 5f;
+
+	public float steerAngle = 10f;
+
+	public float brakeTorque = 50f;
+
+	void Start() {
+		rpm = minRpm;
+		gear = 0;
+	}
+
+	void FixedUpdate() {
+		float steer = Mathf.Clamp(Input.GetAxis("Horizontal"), -1, 1);
+		float gas = Mathf.Clamp(Input.GetAxis("Vertical"), 0, 1);
+		float brake = -1 * Mathf.Clamp(Input.GetAxis("Vertical"), -1, 0);
+
+		if(gas == 0){
+			rpm -= Time.deltaTime * hp;
+			rpm = Mathf.Max (minRpm,rpm);
+		} else {
+			rpm += Time.deltaTime * hp * gas * 4;
+			rpm = Mathf.Min (maxRpm,rpm);
+		}
+		if(rpm == maxRpm && gear < GearRatios.Count-1){
+			rpm = minRpm;
+			gear++;
+		}
+		if(rpm == minRpm && gear > 0){
+			rpm = maxRpm;
+			gear--;
+		}
+		setTorque(getTorque());
+		setSteerAngle(steer*steerAngle);
+		setBrake(brake*brakeTorque);
+		//Debug.Log (Time.deltaTime);
+		Debug.Log("RPM = "+rpm+" - Torque = "+getTorque() + " speed " + rigidbody.velocity.sqrMagnitude + " gear = " + gear );
+	}
+
+	public float getTorque() {
+		return rpm/(axleRatio*GearRatios[gear]*converterRatio);
+	}
+
+	/*
+
 	public float motorTorque = 30f;
 
 	public float steerAngle = 10f;
@@ -15,6 +78,8 @@ public class PlayerController : MonoBehaviour {
 	public float brakeTorque = 50f;
 
 	public float maxSpeed = 50f;
+
+	public float maxSqrMagnitude = 1600f;
 
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -24,11 +89,14 @@ public class PlayerController : MonoBehaviour {
 		float brake = -1 * Mathf.Clamp(Input.GetAxis("Vertical"), -1, 0);
 
 		setSteerAngle(steer * steerAngle);
-		setTorque(motorTorque * motor);
 		setBrake(brake * brakeTorque);
 		Debug.Log(rigidbody.velocity.sqrMagnitude);
+		if(rigidbody.velocity.sqrMagnitude < maxSqrMagnitude)
+			setTorque(motorTorque * motor);
 		audio.pitch = 1 + (forwardWheels[forwardWheels.Count-1].motorTorque) / motorTorque;
 	}
+
+	*/
 
 	void setTorque(float value){
 		for(int i=0;i < forwardWheels.Count;i++){
